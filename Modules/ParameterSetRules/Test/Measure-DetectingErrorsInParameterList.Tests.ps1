b57b91eefc41614e256e4e08fe91351135692858
LaurentDardenne <LaDardenne@Orange.fr>
2016-08-26 19:13:37 +0200

    test Appveyor

--- Modules/ParameterSetRules/Test/Measure-DetectingErrorsInParameterList.Tests.ps1
+++ Modules/ParameterSetRules/Test/Measure-DetectingErrorsInParameterList.Tests.ps1
@@ -289,6 +289,7 @@ Describe "Rule DetectingErrorsInParameterList" {
         $FileName="$Path\Function invalidate 5 rules.ps1"
         
         $Results = Invoke-ScriptAnalyzer -Path $Filename -CustomRulePath $CustomRulePath
+        $Results
         $Results.Count | should be (16)
         $Results[0].Severity| should be 'Error' 
         $Results[1].Severity| should be 'Warning'
