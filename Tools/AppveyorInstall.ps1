3c7fe3c2aa95272f879d45e6995084cad307e102
LaurentDardenne <LaDardenne@Orange.fr>
2016-10-13 14:20:39 +0200

    Remove Install-Module 'PSScriptAnalyzer'

--- Tools/AppveyorInstall.ps1
+++ Tools/AppveyorInstall.ps1
@@ -1,12 +1,9 @@
 ﻿ #nouvelle session pour installer PowershellGet
 PowerShell.exe -Command {
-  Install-PackageProvider Nuget -ForceBootstrap -Force
+  Install-PackageProvider Nuget -ForceBootstrap -Force > $null
   Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
   Install-module PowershellGet -MinimumVersion 1.1.0.0 -Force -Scope AllUsers 
-}
-
- #On utilise la dernière version installé
-Install-Module -Name 'PSScriptAnalyzer' -Repository PSGallery -Scope AllUsers -Force -SkipPublisherCheck
+} 
 
 Install-Module Psake -force
 Import-Module Psake
