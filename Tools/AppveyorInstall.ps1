fc561d691377025b4e70c941e407514d0606d195
LaurentDardenne <LaDardenne@Orange.fr>
2016-10-13 12:53:32 +0200

    fix curly braces problem with YAML

---
+++ Tools/AppveyorInstall.ps1
@@ -0,0 +1,15 @@
+﻿ #nouvelle session pour installer PowershellGet
+PowerShell.exe -Command {
+  Install-PackageProvider Nuget -ForceBootstrap -Force
+  Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+  Install-module PowershellGet -MinimumVersion 1.1.0.0 -Force -Scope AllUsers 
+}
+
+ #On utilise la dernière version installé
+Install-Module -Name 'PSScriptAnalyzer' -Repository PSGallery -Scope AllUsers -Force -SkipPublisherCheck
+
+Install-Module Psake -force
+Import-Module Psake
+
+ #Install/Update required modules
+. "$PSScriptAnalyzerRulesVcs\Tools\Install.ps1"
